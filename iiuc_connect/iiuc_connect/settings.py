51b4a6883057554f5e49a96fd3b561a227c09393
Minhaz <mizbahulaliminhaz350309@gmail.com>
2025-11-21 19:17:30 +0600

    cors update

--- iiuc_connect/iiuc_connect/settings.py
+++ iiuc_connect/iiuc_connect/settings.py
@@ -55,9 +55,11 @@ INSTALLED_APPS = [
     'routine',
     'event',
     'notification',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
